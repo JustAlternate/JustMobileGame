257926084ff43be1d7603f45c0d70cb836906f58
JustAlternate <lololollololol890@gmail.com>
2023-08-19 16:05:09 +0200

    light fixing maybe, objects moving fixing, and buttons scaling

--- Scripts/bullets.gd
+++ Scripts/bullets.gd
@@ -1,19 +1,19 @@
 extends Node2D
 
 var bullet_list:Array
-var speed:float = 2.0
+var speed:int = 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+    pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	bullet_list = get_children()
-	for bullet in bullet_list:
-		bullet.position.y += speed
-		if bullet.position.y > get_viewport_rect().size.y:
-			bullet.queue_free()
-			get_parent().nbr_dodges +=1
-	
+    bullet_list = get_children()
+    for bullet in bullet_list:
+        bullet.position.y += speed * delta
+        if bullet.position.y > get_viewport_rect().size.y:
+            bullet.queue_free()
+            get_parent().nbr_dodges +=1
+    
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -6,18 +6,18 @@ var radius = 190
 var direction:int = 0
 
 func _ready():
-	var speed = 2
-	var angle = -(3*PI)/2
-	var radius = 190
-	var direction:int = 0
-	position.x = 208 + radius * cos(angle) 
-	position.y = 600 + radius * sin(angle)
+    var speed = 2
+    var angle = -(3*PI)/2
+    var radius = 190
+    var direction:int = 0
+    position.x = 208 + radius * cos(angle) 
+    position.y = 600 + radius * sin(angle)
 
-func _physics_process(delta):
-	if direction == -1 and position.x > 15*scale.x :
-		angle += speed * delta
-	elif direction == 1 and position.x < get_viewport_rect().size.x - 15*scale.x : #RIGHT
-		angle -= speed * delta
-	if direction != 0:
-		position.x = 208 + radius * cos(angle) 
-		position.y = 600 + radius * sin(angle)
+func _process(delta):
+    if direction == -1 and position.x > 15*scale.x :
+        angle += speed * delta
+    elif direction == 1 and position.x < get_viewport_rect().size.x - 15*scale.x : #RIGHT
+        angle -= speed * delta
+    if direction != 0:
+        position.x = 208 + radius * cos(angle) 
+        position.y = 600 + radius * sin(angle)

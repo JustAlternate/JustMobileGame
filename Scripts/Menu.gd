4103160466a26489915f0d889949be7d5cf2e0e4
JustAlternate <lololollololol890@gmail.com>
2023-10-05 22:42:41 +0200

    Ajout menu

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -1,5 +1,5 @@
+extends Node2D
 
-extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,6 +11,8 @@ func _process(delta):
 	pass
 
 
-func _on_body_entered(body):
-	get_tree().get_root().get_node("./Main").dying()
-	queue_free()
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+
+func _on_button_2_pressed():
+	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
--- Scripts/leaderboard.gd
+++ Scripts/leaderboard.gd
@@ -4,48 +4,48 @@ var case = preload("res://Scenes/case.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    pass
+	pass
 
 func update_leaderboard():
-    $HTTPRequest.request_completed.connect(_on_request_completed)
-    $HTTPRequest.request("https://justalternate.fr:8082/leaderboard")
+	$HTTPRequest.request_completed.connect(_on_request_completed)
+	$HTTPRequest.request("https://justalternate.fr:8082/leaderboard")
 
 func _on_request_completed(result, response_code, headers, body):
-    var json = JSON.parse_string(body.get_string_from_utf8())
-    draw_leaderboard(json)
+	var json = JSON.parse_string(body.get_string_from_utf8())
+	draw_leaderboard(json)
 
 func draw_leaderboard(leaderboard):
-    clear_leaderboard()
-    var i = 0
-    for player in leaderboard:
-        i+=1
-        var rank_case = case.instantiate()
-        rank_case.text = str(i)
-        var name_case = case.instantiate()
-        name_case.text = str(player[0])
-        var score_case = case.instantiate()
-        score_case.text = str(player[1])
-        var dodge_case = case.instantiate()
-        dodge_case.text = str(player[2])
-        var powerups_case = case.instantiate()
-        powerups_case.text = str(player[3])
-        $ScrollContainer/GridContainer.add_child(rank_case)
-        $ScrollContainer/GridContainer.add_child(VSeparator.new())
-        $ScrollContainer/GridContainer.add_child(name_case)
-        $ScrollContainer/GridContainer.add_child(VSeparator.new())
+	clear_leaderboard()
+	var i = 0
+	for player in leaderboard:
+		i+=1
+		var rank_case = case.instantiate()
+		rank_case.text = str(i)
+		var name_case = case.instantiate()
+		name_case.text = str(player[0])
+		var score_case = case.instantiate()
+		score_case.text = str(player[1])
+		var dodge_case = case.instantiate()
+		dodge_case.text = str(player[2])
+		var powerups_case = case.instantiate()
+		powerups_case.text = str(player[3])
+		$ScrollContainer/GridContainer.add_child(rank_case)
+		$ScrollContainer/GridContainer.add_child(VSeparator.new())
+		$ScrollContainer/GridContainer.add_child(name_case)
+		$ScrollContainer/GridContainer.add_child(VSeparator.new())
 
-        $ScrollContainer/GridContainer.add_child(score_case)
-        $ScrollContainer/GridContainer.add_child(VSeparator.new())
+		$ScrollContainer/GridContainer.add_child(score_case)
+		$ScrollContainer/GridContainer.add_child(VSeparator.new())
 
-        $ScrollContainer/GridContainer.add_child(dodge_case)
-        $ScrollContainer/GridContainer.add_child(VSeparator.new())
+		$ScrollContainer/GridContainer.add_child(dodge_case)
+		$ScrollContainer/GridContainer.add_child(VSeparator.new())
 
-        $ScrollContainer/GridContainer.add_child(powerups_case)
+		$ScrollContainer/GridContainer.add_child(powerups_case)
 
 
 func clear_leaderboard():
-    var list_children = $ScrollContainer/GridContainer.get_children()
-    for i in range(len(list_children)):
-        if i > 8: # Pour pas effacer les placeholders
-            list_children[i].queue_free()
+	var list_children = $ScrollContainer/GridContainer.get_children()
+	for i in range(len(list_children)):
+		if i > 8: # Pour pas effacer les placeholders
+			list_children[i].queue_free()
 

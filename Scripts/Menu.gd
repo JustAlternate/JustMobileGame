33b4287c2872ea6225739d38f8e0069f22b836b0
JustAlternate <lololollololol890@gmail.com>
2023-10-08 00:11:38 +0200

    updating everythin

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -3,16 +3,16 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+    pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+    pass
 
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+    get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
 func _on_button_2_pressed():
-	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
+    get_tree().change_scene_to_file("res://Scenes/adventure.tscn")

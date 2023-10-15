08b1a18926f0ebbbe1e7b46d55c77b470679c2ff
JustAlternate <lololollololol890@gmail.com>
2023-10-15 07:25:18 +0200

    updateee

--- Scripts/choose_lvl.gd
+++ Scripts/choose_lvl.gd
@@ -7,57 +7,57 @@ var insane = 2.3
 var impossible = 2.5
 
 func _on_back_to_menu_pressed():
-    get_tree().change_scene_to_file("res://Scenes/Menu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Menu.tscn")
 
 
 func _on_button_pressed():
-    GlobalVARS.song = "My Chemical Romance - Helena (8-bit Cover).mp3"
-    GlobalVARS.json_file_name = "My Chemical Romance - Helena (8-bit Cover).json"
-    GlobalVARS.diff = normal
-    get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
-    
+	GlobalVARS.song = "My Chemical Romance - Helena (8-bit Cover).mp3"
+	GlobalVARS.json_file_name = "My Chemical Romance - Helena (8-bit Cover).json"
+	GlobalVARS.diff = normal
+	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
+	
 func _on_button_2_pressed():
-    GlobalVARS.song = "Teenage Mutant Ninja Turtles Theme Song 8 Bit Remix Cover Version 8 Bit Universe.mp3"
-    GlobalVARS.json_file_name = "Teenage Mutant Ninja Turtles Theme Song 8 Bit Remix Cover Version 8 Bit Universe.json"
-    GlobalVARS.diff = normal
-    get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
+	GlobalVARS.song = "Teenage Mutant Ninja Turtles Theme Song 8 Bit Remix Cover Version 8 Bit Universe.mp3"
+	GlobalVARS.json_file_name = "Teenage Mutant Ninja Turtles Theme Song 8 Bit Remix Cover Version 8 Bit Universe.json"
+	GlobalVARS.diff = normal
+	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
 
 
 func _on_button_3_pressed():
-    GlobalVARS.song = "te rickrollearon en 8bit.mp3"
-    GlobalVARS.json_file_name = "te rickrollearon en 8bit.json"
-    GlobalVARS.diff = normal
-    get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
-    
+	GlobalVARS.song = "te rickrollearon en 8bit.mp3"
+	GlobalVARS.json_file_name = "te rickrollearon en 8bit.json"
+	GlobalVARS.diff = normal
+	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
+	
 func _on_button_4_pressed():
-    GlobalVARS.song = "Warriors 8 Bit Remix Cover Version Tribute to Imagine Dragons 8 Bit Universe.mp3"
-    GlobalVARS.json_file_name = "Warriors 8 Bit Remix Cover Version Tribute to Imagine Dragons 8 Bit Universe.json"
-    GlobalVARS.diff = hard
-    get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
+	GlobalVARS.song = "Warriors 8 Bit Remix Cover Version Tribute to Imagine Dragons 8 Bit Universe.mp3"
+	GlobalVARS.json_file_name = "Warriors 8 Bit Remix Cover Version Tribute to Imagine Dragons 8 Bit Universe.json"
+	GlobalVARS.diff = hard
+	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
 
 
 func _on_button_5_pressed():
-    GlobalVARS.song = "Gravity Falls Theme 8 Bit Tribute to Gravity Falls 8 Bit Universe.mp3"
-    GlobalVARS.json_file_name = "Gravity Falls Theme 8 Bit Tribute to Gravity Falls 8 Bit Universe.json"
-    GlobalVARS.diff = normal
-    get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
+	GlobalVARS.song = "Gravity Falls Theme 8 Bit Tribute to Gravity Falls 8 Bit Universe.mp3"
+	GlobalVARS.json_file_name = "Gravity Falls Theme 8 Bit Tribute to Gravity Falls 8 Bit Universe.json"
+	GlobalVARS.diff = normal
+	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
 
 func _on_button_6_pressed():
-    GlobalVARS.song = "At Dooms Gate 8 Bit Cover Tribute to Doom 8 Bit Universe.mp3"
-    GlobalVARS.json_file_name = "At Dooms Gate 8 Bit Cover Tribute to Doom 8 Bit Universe.json"
-    GlobalVARS.diff = easy
-    get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
+	GlobalVARS.song = "At Dooms Gate 8 Bit Cover Tribute to Doom 8 Bit Universe.mp3"
+	GlobalVARS.json_file_name = "At Dooms Gate 8 Bit Cover Tribute to Doom 8 Bit Universe.json"
+	GlobalVARS.diff = easy
+	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
 
 
 func _on_button_7_pressed():
-    GlobalVARS.song = "Megalovania Undertale 8 Bit Universe Version.mp3"
-    GlobalVARS.json_file_name = "Megalovania Undertale 8 Bit Universe Version.json"
-    GlobalVARS.diff = insane
-    get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
+	GlobalVARS.song = "Megalovania Undertale 8 Bit Universe Version.mp3"
+	GlobalVARS.json_file_name = "Megalovania Undertale 8 Bit Universe Version.json"
+	GlobalVARS.diff = insane
+	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
 
 
 func _on_button_8_pressed():
-    GlobalVARS.song = "Through The Fire and Flames 8 Bit Cover Tribute to Dragonforce 8 Bit Universe.mp3"
-    GlobalVARS.json_file_name = "Through The Fire and Flames 8 Bit Cover Tribute to Dragonforce 8 Bit Universe.json"
-    GlobalVARS.diff = impossible
-    get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
+	GlobalVARS.song = "Through The Fire and Flames 8 Bit Cover Tribute to Dragonforce 8 Bit Universe.mp3"
+	GlobalVARS.json_file_name = "Through The Fire and Flames 8 Bit Cover Tribute to Dragonforce 8 Bit Universe.json"
+	GlobalVARS.diff = impossible
+	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
--- Scripts/level_1.gd
+++ Scripts/level_1.gd
@@ -17,88 +17,107 @@ var json_as_text = FileAccess.get_file_as_string("res://Assets/"+json_file_name 
 var data = JSON.parse_string(json_as_text)
 var data_index = 0
 
+@onready var MusicPlayer = get_node("MusicPlayer1")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    seed(1)
-    diff = GlobalVARS.diff
-    var file = FileAccess.open("res://Assets/"+GlobalVARS.song, FileAccess.READ)
-    var buffer = file.get_buffer(file.get_length())
-    var stream = AudioStreamMP3.new()
-    stream.data = buffer
-    $MusicPlayer.stream = stream
-    $MusicPlayer.play()
-    $CanvasModulate.visible = true
-    player_instance.speed += 0.5*diff
-    add_child(player_instance)
-    $player.scale = Vector2(2,2)
-    $player/PointLight2D.texture_scale = 6
-    
-    
+	json_file_name = GlobalVARS.json_file_name
+	json_as_text = FileAccess.get_file_as_string("res://Assets/"+json_file_name )
+	data = JSON.parse_string(json_as_text)
+	data_index = 0
+	seed(1)
+	diff = GlobalVARS.diff
+	print(diff)
+	if GlobalVARS.song == "At Dooms Gate 8 Bit Cover Tribute to Doom 8 Bit Universe.mp3":
+		MusicPlayer = get_node("MusicPlayer1")
+	if GlobalVARS.song == "My Chemical Romance - Helena (8-bit Cover).mp3":
+		MusicPlayer = get_node("MusicPlayer2")
+	if GlobalVARS.song == "Gravity Falls Theme 8 Bit Tribute to Gravity Falls 8 Bit Universe.mp3":
+		MusicPlayer = get_node("MusicPlayer3")
+	if GlobalVARS.song == "Teenage Mutant Ninja Turtles Theme Song 8 Bit Remix Cover Version 8 Bit Universe.mp3":
+		MusicPlayer = get_node("MusicPlayer4")
+	if GlobalVARS.song == "te rickrollearon en 8bit.mp3":
+		MusicPlayer = get_node("MusicPlayer5")
+	if GlobalVARS.song == "Warriors 8 Bit Remix Cover Version Tribute to Imagine Dragons 8 Bit Universe.mp3":
+		MusicPlayer = get_node("MusicPlayer6")
+	if GlobalVARS.song == "Megalovania Undertale 8 Bit Universe Version.mp3":
+		MusicPlayer = get_node("MusicPlayer7")
+	if GlobalVARS.song == "Through The Fire and Flames 8 Bit Cover Tribute to Dragonforce 8 Bit Universe.mp3":
+		MusicPlayer = get_node("MusicPlayer8")
+		
+	$CanvasModulate.visible = true
+	player_instance.speed += 0.5*diff
+	add_child(player_instance)
+	$player.scale = Vector2(2,2)
+	$player/PointLight2D.texture_scale = 6
+	
+	MusicPlayer.play()
+	
+	
 func spawn_bullet():
-    spawning_bullet = true
-    var bullet_instance = bullet.instantiate()
-    var bullet_size = 8+(diff*2)
-    bullet_instance.scale.x = bullet_size
-    bullet_instance.scale.y = bullet_size
-    bullet_instance.position.x = randi_range(10,get_viewport_rect().size.x - 10)
-    get_node("bullets").add_child(bullet_instance)
-    var intensity = snapped(data[data_index]["intensity"],0.1)
-    print(1-((intensity+diff)*2)/10)
-    await get_tree().create_timer(max(1-((intensity+diff)*2)/10,0.1)).timeout
-    spawning_bullet = false
+	spawning_bullet = true
+	var bullet_instance = bullet.instantiate()
+	var bullet_size = 8+(diff*2)
+	bullet_instance.scale.x = bullet_size
+	bullet_instance.scale.y = bullet_size
+	bullet_instance.position.x = randi_range(10,get_viewport_rect().size.x - 10)
+	get_node("bullets").add_child(bullet_instance)
+	var intensity = snapped(data[data_index]["intensity"],0.1)
+	await get_tree().create_timer(max(1-((intensity+diff)*2)/10,0.1)).timeout
+	spawning_bullet = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    if $MusicPlayer.get_playback_position() >= data[data_index]["end_time"]:
-        var intensity = snapped(data[data_index]["intensity"],0.1)
-        data_index += 1
-        $bullets.speed = min_speed + intensity*200*diff
-    if $MusicPlayer.get_playback_position() >= data[len(data)-1]["end_time"]:
-        data_index = len(data)-1
-    
-    if not spawning_bullet:
-        spawn_bullet()
+	if MusicPlayer.get_playback_position() >= data[data_index]["end_time"]:
+		var intensity = snapped(data[data_index]["intensity"],0.1)
+		data_index += 1
+		$bullets.speed = min_speed + intensity*200*diff
+	if MusicPlayer.get_playback_position() >= data[len(data)-1]["end_time"]:
+		data_index = len(data)-1
+	
+	if not spawning_bullet:
+		spawn_bullet()
 
-    if Input.is_action_just_pressed("left"):
-        $player.direction = -1
-    if Input.is_action_just_pressed("right"):
-        $player.direction = 1
-    if Input.is_action_just_released("left"):
-        $player.direction = 0
-    if Input.is_action_just_released("right"):
-        $player.direction = 0
+	if Input.is_action_just_pressed("left"):
+		$player.direction = -1
+	if Input.is_action_just_pressed("right"):
+		$player.direction = 1
+	if Input.is_action_just_released("left"):
+		$player.direction = 0
+	if Input.is_action_just_released("right"):
+		$player.direction = 0
 
 func _on_gameover_button_pressed():
-    $MusicPlayer.play()
-    spawning_bullet = true
-    for bullet in get_node("bullets").get_children():
-        bullet.queue_free()
-    $GameOver.visible = false
-    dead = false
-    score = 0
-    data_index=0
-    
+	MusicPlayer.play()
+	spawning_bullet = true
+	for bullet in get_node("bullets").get_children():
+		bullet.queue_free()
+	$GameOver.visible = false
+	dead = false
+	score = 0
+	data_index=0
+	
 
 func dying():
-    if not dead:
-        dead = true
-        $MusicPlayer.stop()
-        $GameOver.visible = true
-        $player/PointLight2D.texture_scale = 10
+	if not dead:
+		dead = true
+		MusicPlayer.stop()
+		$GameOver.visible = true
+		$player/PointLight2D.texture_scale = 10
 
 func _on_left_button_down():
-    $player.direction = -1
+	$player.direction = -1
 func _on_left_button_up():
-    $player.direction = 0
+	$player.direction = 0
 func _on_right_button_down():
-    $player.direction = 1
+	$player.direction = 1
 func _on_right_button_up():
-    $player.direction = 0
+	$player.direction = 0
 
 
 func _on_music_player_finished():
-    get_tree().change_scene_to_file("res://Scenes/Menu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Menu.tscn")
 
 
 func _on_back_to_menu_pressed():
-    get_tree().change_scene_to_file("res://Scenes/Menu.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Menu.tscn")
